d2cd4bc5f6d46175bb5e58497ccfa13e54e4b7e4
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-05-10 01:30:07 -0300

    Tela do index bem evoluido

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
+import path from "path";
 
 const server = express();
 
+server.use(express.static(path.join(__dirname, "..", "public")));
+
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+
 server.get("/", (req: Request, res: Response) => {
-	res.send("Hello World!");
+	res.render("index");
 });
 
 server.listen(process.env.PORT);
